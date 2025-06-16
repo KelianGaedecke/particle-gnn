88808a210f99f24394ce4154876ba9724ff8f3ce
Kelian Gaedecke <keliangaedecke@tsf-484-wpa-2-132.epfl.ch>
2025-06-16 10:25:55 +0200

    add model initial structure

--- src/data_utils.py
+++ src/data_utils.py
@@ -6,17 +6,3 @@ def generate_particles(n=10):
     x = pos.clone()
     batch = torch.zeros(n, dtype=torch.long)
     return Data(x=x, pos=pos, batch=batch)
-
-# src/train.py
-from src.model import ParticleGNN
-from src.data_utils import generate_particles
-
-def main():
-    model = ParticleGNN()
-    data = generate_particles(20)
-    delta = model(data.x, data.pos, data.batch)
-    next_pos = data.pos + delta
-    print("Next position:\n", next_pos)
-
-if __name__ == '__main__':
-    main()
